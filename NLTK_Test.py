dd558a52b95dbfc1c63ccf7c2ef6e27504d85ced
nvargas5 <nv3019@gmail.com>
2019-09-12 19:25:13 -0400

    Update NLTK_Test.py

--- NLTK_Test.py
+++ NLTK_Test.py
@@ -3,10 +3,13 @@ import nltk
 import json
 import pandas as pd
 import time
+<<<<<<< HEAD
 
 from pip._internal.utils import encoding
 
 from main import reddit as reddit
+=======
+>>>>>>> d4f2eebc9319fa2e42c16e953a0f3354fdda946a
 from pathlib import Path
 import re
 
@@ -65,7 +68,7 @@ for word in fdist:
 
 fdist.pprint()
 
-with open(frequency_folder / 'frequencies.json'), 'w', encoding='utf-8') as file:
+with open((frequency_folder / 'frequencies.json'), 'w', encoding='utf-8') as file:
     json.dump(post_info, file)
 
 
