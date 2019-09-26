0cda197886fd0f674788e4994f886ce2eff5a3ba
Josh Brucker <joshbrucker@gmail.com>
2019-09-26 12:49:07 -0400

    Touch up ML algorithm

--- sprint1_scraper.py
+++ sprint1_scraper.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import time
 import os, errno
 
+POSTS_TO_SCRAPE = 1000
 
 def scrape1(subreddit_name, reddit):
     start_time = time.time()
@@ -10,12 +11,11 @@ def scrape1(subreddit_name, reddit):
     scrape_controversial(subreddit_name, reddit)
     scrape_hot(subreddit_name, reddit)
     scrape_top(subreddit_name, reddit)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    print("c'est fini")
-
+    print("Scraping done in %s seconds ---" % (time.time() - start_time))
 
 
 def scrape_new(subreddit_name, reddit):
+    print('Starting New scraping...')
     subreddit = reddit.subreddit(subreddit_name)
     data_folder = Path('subreddits')
     list_type_folder = Path('new')
@@ -23,15 +23,17 @@ def scrape_new(subreddit_name, reddit):
     if not Path(subreddit_path).exists():
         Path(subreddit_path).mkdir(parents=True)
     counter = 0
-    for submission in reddit.subreddit(subreddit.display_name).new(limit=100):
+    for submission in reddit.subreddit(subreddit.display_name).new(limit=POSTS_TO_SCRAPE):
         post_info = {}
         post_info['title'] = submission.title
         post_info["score"] = submission.score
+        post_info['id'] = submission.id
         with open(subreddit_path / (str(counter) + '.json'), 'w', encoding='utf-8') as file:
             json.dump(post_info, file)
         counter += 1
 
 def scrape_controversial(subreddit_name, reddit):
+    print('Starting Controversial scraping...')
     subreddit = reddit.subreddit(subreddit_name)
     data_folder = Path('subreddits')
     list_type_folder = Path('controversial')
@@ -39,15 +41,18 @@ def scrape_controversial(subreddit_name, reddit):
     if not Path(subreddit_path).exists():
         Path(subreddit_path).mkdir(parents=True)
     counter = 0
-    for submission in reddit.subreddit(subreddit.display_name).controversial('month', limit=100):
+    for submission in reddit.subreddit(subreddit.display_name).controversial('year', limit=POSTS_TO_SCRAPE):
         post_info = {}
         post_info['title'] = submission.title
-        post_info["score"] = submission.score
+        print('running')
+        post_info['score'] = submission.score - 100
+        post_info['id'] = submission.id
         with open(subreddit_path / (str(counter) + '.json'), 'w', encoding='utf-8') as file:
             json.dump(post_info, file)
         counter += 1
 
 def scrape_top(subreddit_name, reddit):
+    print('Starting Top scraping...')
     subreddit = reddit.subreddit(subreddit_name)
     data_folder = Path('subreddits')
     list_type_folder = Path('top')
@@ -55,15 +60,17 @@ def scrape_top(subreddit_name, reddit):
     if not Path(subreddit_path).exists():
         Path(subreddit_path).mkdir(parents=True)
     counter = 0
-    for submission in reddit.subreddit(subreddit.display_name).top('week', limit=100):
+    for submission in reddit.subreddit(subreddit.display_name).top('all', limit=POSTS_TO_SCRAPE):
         post_info = {}
         post_info['title'] = submission.title
         post_info["score"] = submission.score
+        post_info['id'] = submission.id
         with open(subreddit_path / (str(counter) + '.json'), 'w', encoding='utf-8') as file:
             json.dump(post_info, file)
         counter += 1
 
 def scrape_hot(subreddit_name, reddit):
+    print('Starting Hot scraping...')
     subreddit = reddit.subreddit(subreddit_name)
     data_folder = Path('subreddits')
     list_type_folder = Path('hot')
@@ -71,10 +78,14 @@ def scrape_hot(subreddit_name, reddit):
     if not Path(subreddit_path).exists():
         Path(subreddit_path).mkdir(parents=True)
     counter = 0
-    for submission in reddit.subreddit(subreddit.display_name).hot(limit=100):
+    for submission in reddit.subreddit(subreddit.display_name).hot(limit=POSTS_TO_SCRAPE):
         post_info = {}
         post_info['title'] = submission.title
-        post_info["score"] = submission.score
+        if submission.score < 100:
+            post_info['score'] = submission.score * 10
+        else:
+            post_info['score'] = submission.score
+        post_info['id'] = submission.id
         with open(subreddit_path / (str(counter) + '.json'), 'w', encoding='utf-8') as file:
             json.dump(post_info, file)
         counter += 1
