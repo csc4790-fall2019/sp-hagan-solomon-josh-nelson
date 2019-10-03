5cf1b90ebc773fd3f47e497f1bae8fab748a5349
nvargas5 <nv3019@gmail.com>
2019-10-03 16:32:44 -0400

    clean1

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index(name):
     return '<h1>hello {}!<h1>'.format(name)
 
+@app.route('/welcome')
+def welcome_page:
+    return '<h1>this is our welcome page {}!<h1>'
+
 
 if __name__ == '__main__':
     app.run()
