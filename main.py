e90ea3d1426ade54fd94eebdd13ecdee7945b89c
Josh Brucker <joshbrucker@gmail.com>
2019-10-23 15:46:18 -0400

    Add run arguments

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import clfs
 from scraper import scrape
 from sklearn.model_selection import train_test_split
 
-def main(runs):
+def main(title_runs, total_percent_runs):
 	with open('auth.json', 'r') as file:
 	    auth = json.load(file)
 
@@ -19,12 +19,12 @@ def main(runs):
 	data = utils.get_data('AskReddit')
 
 	test_title = input('Enter a title: ')
-	prediction = clfs.predict_title(data, test_title, 10)
+	prediction = clfs.predict_title(data, test_title, title_runs)
 	print('Prediction for "{0}": {1}'.format(test_title, prediction))
 
-	success_pct, failure_pct, avg_pct = clfs.get_total_percentages(data, runs)
+	success_pct, failure_pct, avg_pct = clfs.get_total_percentages(data, total_percent_runs)
 	print('Success Posts %: {0}'.format(success_pct))
 	print('Failure Posts %: {0}'.format(failure_pct))
 	print('Average Posts %: {0}'.format(avg_pct))
 
-main(20)
+main(20, 20)
