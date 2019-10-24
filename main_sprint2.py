0b7c82abca6943c83517f9198b56ffc7707fc0cf
nvargas5 <nv3019@gmail.com>
2019-10-24 16:40:48 -0400

    gitignore

---
+++ main_sprint2.py
@@ -0,0 +1,36 @@
+import praw
+import json
+import utils
+import clfs
+import cgi
+import cgitb
+from scraper import scrape
+from sklearn.model_selection import train_test_split
+
+
+def main(runs):
+    with open('auth.json', 'r') as file:
+        auth = json.load(file)
+
+    reddit = praw.Reddit(client_id=auth['client_id'],
+                         client_secret=auth['client_secret'],
+                         username=auth['username'],
+                         password=auth['password'],
+                         user_agent='This is a test.')
+    form = cgi.FieldStorage()
+    title = form.getvalue('title')
+
+    # scrape('AskReddit', reddit)
+    data = utils.get_data('AskReddit')
+
+    test_title = title
+    prediction = clfs.predict_title(data, test_title, 10)
+    print('Prediction for "{0}": {1}'.format(test_title, prediction))
+
+    success_pct, failure_pct, avg_pct = clfs.get_total_percentages(data, runs)
+    print('Success Posts %: {0}'.format(success_pct))
+    print('Failure Posts %: {0}'.format(failure_pct))
+    print('Average Posts %: {0}'.format(avg_pct))
+
+
+main(20)
