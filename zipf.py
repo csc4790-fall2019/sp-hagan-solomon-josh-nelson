a558b87fee73c515cb021b88c1ab201fdbe4d255
nvargas5 <nv3019@gmail.com>
2019-11-14 17:17:20 -0500

    scuffed code sprint 3

--- zipf.py
+++ zipf.py
@@ -31,11 +31,32 @@ def populate_freq_dist_stop_list(subreddit_name):
                         else:
                             freq_dist[token] = 1
 
-    sorted_freq_dist = freq_dist.most_common()
+#starting to add sketchy stuff
+
+    sorted_freq_dist = collections.OrderedDict(freq_dist.most_common())
+
+    num_top_10 = round(len(sorted_freq_dist) * 0.1)
+    num_top_25 = round(len(sorted_freq_dist) * 0.25)
+    top_10_percent = []
+    top_25_percent = []
+
+    counter = 0
+
+    for key, value in sorted_freq_dist.items():
+        counter += 1
+        if key in sorted_freq_dist.keys():
+            if counter <= num_top_10:
+                top_10_percent.append(key)
+        if key in sorted_freq_dist.keys():
+            if counter <= num_top_25:
+                top_25_percent.append(key)
+
+#end sketchy stuff
+
     with open((fd_path / '{0}_freq_dist.json'.format(subreddit_name)), 'w', encoding='utf-8') as file:
         json.dump(sorted_freq_dist, file)
 
-    size ={'size': len(sorted_freq_dist)}
+    size ={'size': len(sorted_freq_dist), 'top_10' : top_10_percent, 'top_25' : top_25_percent}
 
     with open((fd_path / '{0}_freq_dist_size.json'.format(subreddit_name)), 'w', encoding='utf-8') as file:
         json.dump(size, file)
@@ -44,20 +65,38 @@ def populate_freq_dist_stop_list(subreddit_name):
 
 def determine_top_words(title, subreddit):
 
-    subreddit_dist_size = Path('subreddits_freq_dist/{}'.format(subreddit))
+    subreddit_dist_size = Path('subreddits_freq_dist/{}_freq_dist_size.json'.format(subreddit))
+    subreddit_dist = Path('subreddits_freq_dist/{}_freq_dist.json'.format(subreddit))
+
 
     with open(subreddit_dist_size) as f:
-        data = json.load(f)
+        size_data = collections.OrderedDict(json.load(f))
+        length_of_dict = size_data['size']
 
-    holder = re.sub(r'\b[A-Z]+\b', '', title)
-    holder = str.split()
+    with open(subreddit_dist) as g:
+        data = collections.OrderedDict(json.load(g))
+
+    re.sub(r'\b[A-Z]+\b', '', title)
+    title = title.split()
 
     top_10_percent = {}
     top_25_percent = {}
 
-    for token in holder:
+    num_top_10 = round(length_of_dict * 0.1)
+    num_top_25 = round(length_of_dict * 0.25)
 
+    for token in title:
+        for key, value in data.items():
+            if token in data.keys():
+                if data.index(token) <= num_top_10:
+                    top_10_percent.add(token)
+            if key in data.keys():
+                if data.keys().index(token) <= num_top_25:
+                    top_25_percent.add(token)
 
+    print(top_10_percent)
+    print(top_25_percent)
 
+populate_freq_dist_stop_list('AskReddit')
 
-#populate_freq_dist_stop_list('AskReddit')
+#determine_top_words('What it be people made random fluff noise', 'AskReddit')
