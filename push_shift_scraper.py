6c72a43ffcbdab12073f0079e57bf3e4fd57a0f9
Josh Brucker <joshbrucker@gmail.com>
2019-11-13 22:58:49 -0500

    Update scraper

--- push_shift_scraper.py
+++ push_shift_scraper.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from time import time
 import time
 import requests
 
@@ -13,13 +14,35 @@ def scrape(after, before, subreddit, size):
           .format(size= size, after= after, before= before, subreddit= subreddit))
 
     api_url = 'http://api.pushshift.io/reddit/search/submission/?subreddit=' + subreddit + '&after=' + after \
-              + '&before=' + before + '&size=' + size
+              + '&before=' + before + '&size=' + size + '&sort=desc&sort_type=score'
 
     request = requests.get(api_url)
-    data = json.loads(request.text)
+    try:
+        data = json.loads(request.text)
+    except:
+        print('Exception')
+        return
 
-    print(data)
+    path = Path('subreddits/{0}'.format(subreddit.lower()))
+    if not path.exists():
+        path.mkdir(parents=True)
 
+    for submission in data['data']:
+        post_data = {
+            'id': submission['id'],
+            'title': submission['title'],
+            'score': submission['score'],
+        }
 
+        with open(path / '{0}.json'.format(post_data['id']), 'w', encoding='utf-8') as file:
+            json.dump(post_data, file)
 
-scrape('100d', '90d' , 'askreddit', '100')
+# scrape('20d', '0d', 'askreddit', '500')
+
+before = 0
+after = 20
+for x in range(50):
+    scrape('{0}d'.format(after), '{0}d'.format(before), 'askreddit', '500')
+    before += 20
+    after += 20
+    time.sleep(1)
